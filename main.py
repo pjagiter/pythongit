edf277223ba58ed2b9e6a36a7be6cfee58a39d3f
pjagiter <kwasniewiczl@pjwstk.edu.pl>
2023-03-10 19:33:31 +0100

    add script for open webarchive's pages. add mp3 for .gitignore add new empty script

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 # This is a sample Python script.
 import webbrowser
-pageUrl = 'https://github.com'
-webbrowser.open(pageUrl)
+import requests
+
+#pageurl = 'https://github.com'
+#date = 20150101
+pageurl = input("podaj adres strony: ")
+date = input("podaj date np. 20230130")
+url = 'http://archive.org/wayback/available?url=' + pageurl + '&timestamp=' + str(date)
+response = requests.get(url)
+d = response.json()
+
+page = d["archived_snapshots"]["closest"]["url"]
+webbrowser.open(page)
 
